88938763faf96732fb333864a8cdb81dcc5c7989
kileymatschke <matschke.kiley@gmail.com>
2026-01-29 19:06:24 -0500

    week 1: hello world next.js app

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+export default function Home() {
+    return (
+        <main className="min-h-screen flex flex-col items-center justify-center text-center">
+            <h1 className="text-4xl font-bold mb-4">The Humor Project</h1>
+            <p className="text-xl">Week 1: Hello World</p>
+            <p className="text-xl">Kiley Matschke
+
+            </p>
+        </main>
+    );
+}
